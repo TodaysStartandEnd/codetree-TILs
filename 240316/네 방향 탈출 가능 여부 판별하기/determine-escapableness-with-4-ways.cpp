d7f0cd15b8d121637b975546377491870fac2a24
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-16 23:29:05 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240316/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int a[101][101];
+int check[101][101];
+int x[4]={1,-1,0,0};
+int y[4]={0,0,1,-1};
+bool flag = 0;
+
+int main() {
+    int n, m;
+    queue<pair<int,int>> q;
+    cin >> n >> m ;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    q.push({0,0});
+    check[0][0] =1;
+    while(!q.empty())
+    {
+        int a = q.front().first;
+        int b = q.front().second;
+        q.pop();
+        for(int i=0; i<4; i++)
+        {
+            int nx = a + x[i];
+            int ny = b + y[i];
+            if(check[nx][ny]==1 || nx <0 || nx >=n || ny <0 || ny >=m)
+            {
+                continue;
+            }
+            else
+            {
+                if(nx == n-1 && ny ==m-1)
+                {
+                    flag =1 ;
+                    break;
+                }
+                check[nx][ny] = 1;
+                q.push({nx,ny});
+            }
+        }
+
+    }
+    if(flag == 1)
+    {
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}
