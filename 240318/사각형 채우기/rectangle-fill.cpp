81f0d10dab4b04aeb20f5a24eaafe272550ef2bb
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-18 20:18:39 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int a[1000];
+
+int main() 
+{
+    int n ;
+    cin >> n;
+    // 홀수 : 앞의 짝수 + a[1]
+    // 짝수 : 앞의 짝수 + a[2]
+    a[1] = 1;
+    a[2] = 2;
+    for(int i=3; i<=n ; i++)
+    {
+        if(i%2==0)
+        {
+            a[i]= a[i-2] + a[2];
+        }
+        else
+        {
+            a[i] = a[i-1]+a[1];
+        }
+    }
+    cout << a[n];
+
+    return 0;
+}
