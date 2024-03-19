6ba6da45eb2d850a4546a97f878d86da2d7ecca4
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-19 22:30:51 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240319/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240319/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+//1 : 2
+//2 : 7
+//3 : a[2]*a[1](1x2 넣기(세로)) + a[1]*3(2x2 가로 넣기) + 2
+
+
 int a[1001];
 
 int main() {
@@ -10,7 +15,7 @@ int main() {
     a[2] = 7;
     for(int i=3; i<=n;i++)
     {
-        a[i]= (((3*a[i-1])%1000000007)+1)%1000000007;
+        a[i]= (a[i-1]*a[1]+a[i-2]*a[2]+2)%1000000007;
     }
     cout << a[n];
     return 0;
