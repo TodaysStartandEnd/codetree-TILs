c9d2e72b2cbca20e889b44a8865aa741657f0a1a
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-20 22:57:39 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/사각형 채우기 2/rectangle-fill-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+
+int main() {
+    int n;
+    cin >> n;
+    a[1]=1;
+    a[2]=3;
+    for(int i=3;i<=n;i++)
+    {
+        a[i]= a[i-1]+2*a[i-2];
+    }
+    cout << a[n];
+    return 0;
+}
