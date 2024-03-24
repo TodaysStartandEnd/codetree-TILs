b47f4870c8dcfcc026f51236503d11a2b55d2f61
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-24 22:45:47 +0900

    Title: 동전 거슬러주기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240324/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int a[101];
+int dp[101];
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    for(int i=0; i<=m; i++)
+    {
+        dp[i] = -1;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+        dp[a[i]] = 1;
+    }
+    for(int i=1; i<=m; i++)
+    {
+        for(int j=0; j<n;j++)
+        {
+            if(dp[i- a[j]]>0)
+            {
+                if(dp[i] == -1)
+                {
+                    dp[i] = dp[i-a[j]]+1;
+                }
+                else
+                {
+                    dp[i] = min(dp[i-a[j]]+1,dp[i]);
+                }
+            }
+        }
+    }
+ 
+    cout << dp[m];
+    return 0;
+}
