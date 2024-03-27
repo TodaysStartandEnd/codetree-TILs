99b9b3ac45f8a608607b03fcc4b6ae0911d36557
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-03-27 22:21:05 +0900

    Title: 최대 동전 거슬러주기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240327/최대 동전 거슬러주기/max-coin-change.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[101];
+int dp[101];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+     for(int i=0; i<=n;i++)
+    {
+        dp[i] = -1;
+    }
+    for(int i=0; i<n;i++)
+    {
+        cin >> a[i];
+        dp[a[i]] = 1;
+    }
+    for(int i=1; i<=m; i++)
+    {
+        for(int j=0; j<n;j++)
+        {
+            if(i-a[j]>=1 && i-a[j] <= m)
+            {
+                dp[i] = max(dp[i-a[j]]+1, dp[i]);
+            }
+        }
+    }
+    cout << dp[m];
+    return 0;
+}
