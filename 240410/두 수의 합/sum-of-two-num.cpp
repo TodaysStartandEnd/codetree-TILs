6962499ee596464166924588e24c875a0ff6bf4b
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-04-10 23:59:15 +0900

    Title: 두 수의 합, Time: 100ms, Memory: 4MB, Status: Passed - CodeTree

--- 240410/두 수의 합/sum-of-two-num.cpp
+++ 240410/두 수의 합/sum-of-two-num.cpp
@@ -2,41 +2,42 @@
 #include <map>
 using namespace std;
 
-int main() 
-{
+int main() {
     map<int,int> map;
-    long n, k;
+    int n, k;
     long count = 0;
 
     cin >> n >> k;
 
-    for(int i=0; i<n; i++)
+    for(int i = 0; i < n; i++) 
     {
         int x;
         cin >> x;
-        if(map.find(x) != map.end())
+        if(map.find(x) != map.end()) 
         {
             map[x]++;
-        }
-        else
+        } else 
         {
-            map.insert({x,1});
+            map.insert({x, 1});
         }
-        
     }
-    for(auto i=map.begin(); i!=map.end(); i++)
+
+    for(auto i = map.begin(); i != map.end(); i++) 
     {
-        if(map.find(k-((i)->first))!=map.end())
+        if(map.find(k - i->first) != map.end() && map[k - i->first] > 0) 
         {
-            if((map.find(k-(i)->first))->second != 0) 
+            if (k - i->first == i->first) 
             {
-                i->second --;
-                count += i->second;
+                count += (i->second * (i->second - 1)) / 2;
+            } else 
+            {
+                count += i->second * map[k - i->first];
             }
-           
+            map[i->first] = 0;
+            map[k - i->first] = 0;
         }
-        
     }
+
     cout << count;
 
     return 0;
