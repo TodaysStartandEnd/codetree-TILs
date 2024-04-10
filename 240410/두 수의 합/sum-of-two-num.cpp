26e994f34c4ba8a4246e7d3734b38afa06e1143c
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-04-10 23:42:00 +0900

    Title: 두 수의 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240410/두 수의 합/sum-of-two-num.cpp
+++ 240410/두 수의 합/sum-of-two-num.cpp
@@ -14,16 +14,23 @@ int main()
     {
         int x;
         cin >> x;
-        map.insert({x,0});
+        if(map.find(x) != map.end())
+        {
+            map[x]++;
+        }
+        else
+        {
+            map.insert({x,1});
+        }
+        
     }
     for(auto i=map.begin(); i!=map.end(); i++)
     {
         if(map.find(k-(i)->first)!=map.end())
         {
-            if((map.find(k-(i)->first))->second == 0) // k합 만드는 다른 수가 먼저 존재했어서 체크 된 경우
+            if((map.find(k-(i)->first))->second != 0) // k합 만드는 다른 수가 먼저 존재했어서 체크 된 경우
             {
-                i->second = 1;
-                (map.find(k-(i)->first))->second = 1;
+                i->second --;
                 count ++;
             }
             
