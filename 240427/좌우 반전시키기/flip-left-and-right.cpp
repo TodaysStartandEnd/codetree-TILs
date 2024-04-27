07709bd9ebd8d1e4ce4ed2ed0028bfce348ae279
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-04-27 22:08:13 +0900

    Title: 좌우 반전시키기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240427/좌우 반전시키기/flip-left-and-right.cpp
+++ 240427/좌우 반전시키기/flip-left-and-right.cpp
@@ -11,30 +11,39 @@ int main()
     {
         cin >> a[i];
     }
-    for(int i=1; i<n-1;i++)
+    if(n==1)
     {
-        if(a[i-1]==0)
-        {
-            count++;
-            a[i-1] = 1;
-            a[i] = !a[i];
-            a[i+1] = !a[i+1];
-        }
+        count = -1;
     }
-    if(a[n-2]==0)
+    else
     {
+        for(int i=1; i<n-1;i++)
+        {
+            if(a[i-1]==0)
+            {   
+                count++;
+                a[i-1] = 1;
+                a[i] = !a[i];
+                a[i+1] = !a[i+1];
+            }
+        }
+        if(a[n-2]==0)
+        {
         count++;
         a[n-2]= 1;
         a[n-1]= !a[n-1];
-    }
-    for(int i=0; i<n;i++)
-    {
-        if(a[i]!=1)
-        {
-            count = -1;
-            break;
         }
+        for(int i=0; i<n;i++)
+        {
+            if(a[i]!=1)
+            {
+                count = -1;
+                break;
+            }
+        }
+
     }
+    
     cout << count;
     
     return 0;
