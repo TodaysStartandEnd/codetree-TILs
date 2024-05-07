e47572ec74ecb27bb23ba641f15312a2f49576cf
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-07 23:21:45 +0900

    Title: 낮은 지점들, Time: 54ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240507/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() 
+{
+    unordered_map<int, int> umap;
+    int n;
+    int max = 0;
+    cin >> n;
+    while(n--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if(umap.find(x)!=umap.end())
+        {
+            if(umap[x]>y)
+            {
+                umap[x] = y;
+            }
+        }
+        else
+        {
+            umap[x] = y;
+        }
+    }
+    for(auto i = umap.begin(); i!=umap.end(); i++)
+    {
+        max += i->second;
+    }
+    cout << max;
+    return 0;
+}
