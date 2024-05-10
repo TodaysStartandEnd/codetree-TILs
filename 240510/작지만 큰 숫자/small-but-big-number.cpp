4c75521a9961f393c398ae2a6a08a7b2a0daae18
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-10 22:51:52 +0900

    Title: 작지만 큰 숫자, Time: 274ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 240510/작지만 큰 숫자/small-but-big-number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() 
+{
+    set <int> set;
+    int n , m;
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+    {
+        int x; 
+        cin >> x;
+        set.insert(-x);   
+    }
+    for(int i=0; i<m; i++)
+    {
+        int x;
+        cin >> x;
+        if(set.lower_bound(-x)!=set.end())
+        {
+            cout <<-*set.lower_bound(-x)<<"\n";
+            set.erase(*set.lower_bound(-x));
+        }
+        else
+        {
+            cout <<-1<<"\n";
+        }
+        
+    }
+    return 0;
+}
