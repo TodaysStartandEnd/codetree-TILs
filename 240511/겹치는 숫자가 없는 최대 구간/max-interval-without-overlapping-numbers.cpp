d7b410c439c4d0dd044f7e355fbdf65c5cdf1351
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-11 23:28:34 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 17ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240511/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.cpp
+++ 240511/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.cpp
@@ -6,6 +6,7 @@ int main()
     int n;
     int count[100001];
     int a[100001];
+    int b[100001];
     int size = 0;
     int max =0;
     cin >> n;
@@ -15,19 +16,26 @@ int main()
     }
     for(int i=0; i<n; i++)
     {
+        if(max <size)
+        {
+            max = size;
+        }
         count[a[i]]++;
+        if(count[a[i]] == 1)
+        {
+            b[a[i]] =i;
+        }
         if(count[a[i]]>1)
         {
             count[i]--;
+            size -=b[a[i]];
+            b[a[i]] = i;
         }
         else
         {
             size++;
         }
-        if(max <size)
-        {
-            max = size;
-        }
+        
     }
     cout << max;
     
