3595455f6aa7010393593562d0314216f7f1cd77
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-11 20:07:26 +0900

    Title: 차이가 가장 작은 수, Time: 1094ms, Memory: 52MB, Status: Time Limit Exceed - CodeTree

---
+++ 240511/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    int n, m;
+    cin >> n >> m;
+    int a[100001];
+    set <int> diff;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+        for(int j=0;j<i; j++)
+        {
+            diff.insert(abs(a[i]-a[j]));
+        }
+    }
+
+    if(diff.lower_bound(m)!=diff.end())
+    {
+        cout << *diff.lower_bound(m) ;
+    }
+    else
+    {
+        cout <<-1;
+    }
+    return 0;
+}
