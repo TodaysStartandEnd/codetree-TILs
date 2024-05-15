60813c0017b52408070d34c61d470ffa3371b171
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-15 22:22:02 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240515/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() 
+{
+    list<char> li;
+    list<char>::iterator it = li.end();
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    for(int i=0; i<n; i++)
+    {
+        li.push_back(s[i]);   
+    }
+    while(m--)
+    {
+        string order;
+        cin >> order;
+        if(order == "L")
+        {
+            if(it != li.begin())
+            {
+                it--;
+            }
+        }
+        else if(order == "R")
+        {
+            if(it != li.end())
+            {
+                it++;
+            }
+        }
+        else if(order == "D")
+        {
+            if(it != li.end())
+            {
+                li.erase(it);
+            }
+        }
+        else if(order == "P")
+        {
+            char x;
+            cin >> x; 
+            li.insert(it, x);
+            
+        }
+
+    }
+    for(list<char>::iterator it = li.begin(); it!=li.end();it++)
+    {
+        cout << *it;
+    }
+    
+
+    return 0;
+}
