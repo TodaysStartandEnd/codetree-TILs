4c010c2b0f2383fb1f3c3ff25a02f5bc75b85e6d
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-23 22:07:54 +0900

    Title: 정수 명령 처리 2, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240523/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() 
+{
+    queue<int> q;
+    int n;
+    cin >> n ;
+    while (n--)
+    {
+        string s;
+        cin >> s; 
+        if(s == "push")
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if(s =="pop")
+        {
+            cout << q.front() <<'\n';
+            q.pop();
+        }
+        else if(s == "size")
+        {
+            cout << q.size() <<'\n';
+        }
+        else if(s == "empty")
+        {
+            cout << q.empty() ? 1 : 0 ;
+            cout <<'\n';
+        }
+        else if(s == "front")
+        {
+            cout << q.front() <<'\n';
+        }
+    }
+    return 0;
+}
