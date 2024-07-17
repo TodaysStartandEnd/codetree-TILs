65865ddb0541d3e9e223509dac8e555b34a29b9d
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-07-17 17:30:54 +0900

    Title: 수들의 합 최대화하기, Time: 1097ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240717/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int a[11][11];
+bool row[11];
+bool high[11];
+long max_sum = 0;
+int n;
+
+
+void Choose(long now, int cnt)
+{
+    if(cnt == n)
+    {
+        max_sum = max(max_sum, now);
+    }
+    for(int i=0; i<n ; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if((row[i] !=1) && (high[j] !=1))
+            {
+                row[i] = 1;
+                high[j] = 1;
+                Choose(now+a[i][j], cnt+1);
+                row[i] = 0;
+                high[j] = 0;
+            }
+
+        }
+    }
+}
+
+int main() 
+{
+    cin >> n; 
+    for(int i=0; i<n ; i++)
+    {
+        for(int j=0; j<n ; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    Choose(0,0);
+    cout << max_sum <<'\n';
+    
+    return 0;
+}
