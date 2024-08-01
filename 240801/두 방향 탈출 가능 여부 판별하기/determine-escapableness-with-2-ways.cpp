32e0438b8d2276a6a825a8c8a688ea4a194abee6
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-08-01 22:14:25 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+bool check = 0;
+bool visit[101][101];
+int n,m;
+
+void DFS(int x, int y)
+{
+    int dx[2] = {1,0};
+    int dy[2] = {0,1};
+
+    
+    visit[x][y] = 1;
+
+    if(visit[n-1][m-1] == 1)
+    {
+        check = 1;
+        return;
+
+    }
+    for(int i=0; i<2; i++)
+    {
+        int nx = y+dx[i];
+        int ny = y+dy[i];
+        if(nx<n&&ny<m&&visit[nx][ny]==0)
+        {
+            DFS(nx,ny);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m ;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin >> visit[i][j];
+        }
+    }
+    if(visit[n-1][m-1] ==1)
+    {
+        cout << 0 <<'\n';
+    }
+    else
+    {
+        DFS(0,0);
+        cout << check <<'\n';
+    }
+    return 0;
+}
