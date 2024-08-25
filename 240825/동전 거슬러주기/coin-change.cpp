745614e9ba808274e5093934d7354e1c1a3e9f44
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-08-25 20:09:26 +0900

    Title: 동전 거슬러주기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240825/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int a[101];
+long dp[10001];
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    for(int i=0; i<=m; i++)
+    {
+        dp[i] = 0;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+        dp[a[i]] = 1;
+    }
+    for(int i=1; i<=m; i++)
+    {
+        for(int j=0; j<n;j++)
+        {
+            if(i<a[j])
+            {
+                continue;
+            }
+            if(dp[i- a[j]]>=0)
+            {
+                if(dp[i] == 0)
+                {
+                    dp[i] = dp[i-a[j]]+1;
+                }
+                else
+                {
+                    dp[i] = min(dp[i-a[j]]+1,dp[i]);
+                }
+            }
+        }
+    }
+ 
+    cout << dp[m];
+    return 0;
+}
