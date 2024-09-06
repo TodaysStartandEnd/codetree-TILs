3aaae48be5a1c8ba85f2c3cf414da5c40740a2be
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-09-06 14:24:20 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int g[1001][1001];
+bool v[1001];
+int n, m;
+int count =0;
+
+
+void dfs(int a)
+{
+    for(int i=1; i<=n; i++)
+    {
+        if(g[a][i]==1&&v[i] == 0)
+        {
+            v[i]= 1;
+            count ++;
+            dfs(i);
+            
+        }
+    }
+}
+
+int main()
+{
+    
+    cin >> n >> m;
+    for(int i=0;i<m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        g[x][y]=1;
+        g[y][x]=1;
+    }
+    for(int i=1; i<=n ; i++)
+    {
+        g[i][0]= 0;
+    }
+    v[1] = 0;
+    dfs(1);
+    cout << count-1 ;
+}
